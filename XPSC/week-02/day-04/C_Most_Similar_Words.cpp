f75a085a785ba1f0f42e6ee9df5098dff6009c15
Ramid <118038450+NMRanaHamid@users.noreply.github.com>
2023-10-09 21:30:40 +0600

    Add files via upload

---
+++ XPSC/week-02/day-04/C_Most_Similar_Words.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+void solve()
+{
+     int n,m,k,i,j; cin>>n>>m;
+     vector<string> s(n);
+     fori(i,0,n) cin>>s[i];
+    //  for(auto y : s) cout<<
+     int ans = INT_MAX ;
+     fori(i,0,n-1)
+     {
+        fori(j,i+1,n)
+        {
+            string a=s[i],b=s[j];
+            // cout<<a<<" "<<b<<nl;
+            int sum = 0;
+            fori(k,0,m)
+            {
+                sum+=(abs(a[k]-b[k]));
+            }
+            ans = min(ans,sum);
+        }
+     }
+     cout<<ans<<nl;
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+     cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
