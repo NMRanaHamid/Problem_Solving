17f8345f0660a4e4b46aa502ee8c3e326871afca
Ramid <118038450+NMRanaHamid@users.noreply.github.com>
2023-10-22 23:17:30 +0600

    Add files via upload

---
+++ XPSC/week-04/day-02/A_Compare_T_Shirt_Sizes.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+void solve()
+{
+     string a,b;
+     cin>>a>>b;
+     char ac = a.back(), bc=b.back();
+     int n = a.size(), m = b.size();
+     if(ac==bc and n==m) cout<<"="<<nl;
+     else if(ac>bc or (ac==bc and ac=='S' and n>m) or (ac==bc and ac=='L' and n<m)) cout<<'<'<<nl;
+     else cout<<">"<<nl;
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+     cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
