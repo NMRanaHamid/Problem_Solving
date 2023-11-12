603063f086adff69b2b3890acd50557cb35ed731
Ramid <118038450+NMRanaHamid@users.noreply.github.com>
2023-11-12 22:35:07 +0600

    Add files via upload

---
+++ XPSC/week-07/day-03/R_Four_Tickets.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int x; cin>>x;
+        cout<<((x*4<=1000)?"YES":"NO")<<endl;
+    }
+	return 0;
+}
