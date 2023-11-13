d1e8a2e9a0c8457612bd07e7b8cfe89913385ae8
Ramid <118038450+NMRanaHamid@users.noreply.github.com>
2023-11-13 23:21:35 +0600

    Add files via upload

---
+++ XPSC/week-07/day-04/AD_Two_Out_of_Three.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<string> vss;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+void solve()
+{
+     int n,m,i,j; cin>>n;
+     map<int,vii> mp;
+     vii a(n),b(n,1); fori(i,0,n) cin>>a[i],mp[a[i]].pb(i);
+     int ok = 0;
+     for(auto a : mp)
+     {
+        int x = a.ss.size();
+        vii v = a.ss;
+        if(x>=2)
+        {
+            if(ok==0) b[v[x-1]] = 2;
+            else b[v[x-2]]=3;
+            ok++;
+        }
+     }
+     if(ok>=2) 
+     {
+        for(auto a : b) cout<<a<<" ";
+        cout<<nl;
+     }
+     else cout<<-1<<nl;
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+     cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
---
+++ XPSC/week-07/day-04/A_T_1.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<string> vss;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+ll l_cm(ll a, ll b)
+{
+    return ((a/__gcd(a,b))*b);
+}
+
+void solve()
+{
+     ll n,a,b,p,q,i,j; cin>>n>>a>>b>>p>>q;
+     ll ans = ((n/a)*p);
+     ans+=((n/b)*q);
+     ll lcm = l_cm(a,b) ;
+     ll same = (n/lcm);
+     ans-=(same*min(p,q));
+     cout<<ans<<nl;
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+    //  cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
