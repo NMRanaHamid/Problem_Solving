897bc91640cdac1e51a7295231e0f2fb8adc500b
Ramid <118038450+NMRanaHamid@users.noreply.github.com>
2023-11-18 18:19:29 +0600

    Add files via upload

---
+++ XPSC/week-08/day-01/A_We_Were_Both_Children.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<string> vss;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+void solve()
+{
+     int n,i,j,k; cin>>n;
+     vii fr(n+2,0);
+     fori(i,0,n)
+     {
+        cin>>k;
+        if(k>n) continue;
+        fr[k]++;
+     }
+     int ans = INT_MIN;
+     fori(i,1,n+1)
+     {
+        int x = 0;
+        for(j=1; j*j<=i; j++)
+        {
+            if(i%j==0)
+            {
+                x+=fr[j];
+                if(j*j!=i) x+=fr[i/j];
+            }
+        }
+        // cout<<x<<" ";
+        ans = max(ans,x);
+     }
+     cout<<ans<<nl;
+
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+     cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
