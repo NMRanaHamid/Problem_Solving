bc0c985ea9e88f5c4247f14ba179e5f2cdc8480f
Ramid <118038450+NMRanaHamid@users.noreply.github.com>
2023-12-11 22:25:39 +0600

    Add files via upload

---
+++ XPSC/week-11/day-03/C_Divisor_Chain.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<string> vss;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+void solve()
+{
+     ll x,i,j; cin>>x;
+     vll ans ;
+     ans.pb(x);
+     if(x&1) x--,ans.pb(x);
+     while(x>2)
+     {
+        ll p = 1;
+        while(x%(2*p)==0) p*=2;
+        if(x==p) p/=2;
+        x-=p;
+        ans.pb(x);
+     }
+     if(x!=1) ans.pb(1);
+     cout<<ans.size()<<nl;
+     for(auto a : ans) cout<<a<<" ";
+     cout<<nl;
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+     cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
