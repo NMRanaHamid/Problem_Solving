b5cdb29e442f0eb30fe8e969ee6a4e878d315c4f
Ramid <118038450+NMRanaHamid@users.noreply.github.com>
2023-12-16 23:31:22 +0600

    Add files via upload

---
+++ XPSC/week-11/day-05/P_Removing_Smallest_Multiples.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<string> vss;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+void solve()
+{
+     int n,i,j; cin>>n;
+     string s; cin>>s;
+     ll ans = 0;
+     vll cost(n);
+     for(i=n; i>=1; i--)
+     {
+        for(j=i-1; j<n; j+=i)
+        {
+            if(s[j]=='1') break;
+            else cost[j]=i;
+        }
+     }
+     fori(i,0,n+1)
+     {
+        if(s[i]=='0') ans+=cost[i];
+     }
+     cout<<ans<<nl;
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+     cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
---
+++ XPSC/week-11/day-05/T_Kalindrome_Array.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// rename macro of operations using typedef
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef pair<double,double> pdd;
+typedef vector<ll> vll;
+typedef vector<int> vii;
+typedef vector<string> vss;
+typedef vector<pair<int,int>> vpi;
+typedef vector<pair<ll,ll>> vpl;
+typedef vector<vector<int> > vvii;
+typedef vector<vector<ll> > vvll;
+
+// rename macro of opearations using define
+#define fori(i,s,n) for(i=s; i<n; i++)
+#define nl "\n"
+#define dbg(x) cout<<#x<<" = "<<x<<ln
+#define mp make_pair
+#define pb push_back
+#define ppb pop_back
+#define ff first
+#define ss second
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sij(x) ((ll)(x).size())
+
+// const values 
+ll Mod = 1e9+7;
+int mod = 1e9+7;
+double pie = 3.141592653589793 ;
+int inf = INT_MAX;
+ll Inf = 1e18 ;
+
+/********define global variable here********/
+
+/*******************Code Start from here**********/
+
+void solve()
+{
+     int n,i,j; cin>>n;
+     vii ar(n); fori(i,0,n) cin>>ar[i];
+     vii b = ar ,ans ;
+     int x ,y;
+     i=0,j=n-1;
+     while(i<=j)
+     {
+        if(ar[i]!=ar[j])
+        {
+            x = ar[i];
+            y=ar[j];
+            break;
+        }
+        i++,j--;
+     }
+    //  cout<<x<< " "<<y<<nl;
+     fori(i,0,n) if(ar[i]!=x) ans.pb(ar[i]);
+     i=0,j=ans.size()-1;
+    //  for(auto a : ans) cout<<a<<" ";
+     bool ok = 1, check=1;
+     while(i<=j)
+     {
+        if(ans[i]!=ans[j])
+        {
+            ok=0;
+            break;
+        }
+        i++,j--;
+     }
+     ans.clear();
+     fori(i,0,n) if(ar[i]!=y) ans.pb(ar[i]);
+     i=0,j=ans.size()-1;
+     while(i<=j)
+     {
+        if(ans[i]!=ans[j])
+        {
+            check=0;
+            break;
+        }
+        i++,j--;
+     }
+     if(check or ok) cout<<"YES"<<nl;
+     else cout<<"NO"<<nl;
+}
+
+int32_t main()
+{
+     fast_cin();
+
+     int t=1;
+     cin >> t;
+     for(int it=1;it<=t;it++)
+     {
+        // cout << "Case #" << it << ": ";
+        solve(); 
+     }
+
+   return 0;
+}
